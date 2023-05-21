bd5a3fd353758b423d88ba2b776317943dda4ffa
Amelia <53657436+AmeliaTYR@users.noreply.github.com>
2023-05-21 11:24:29 +0800

    Update navbar

--- mentorme/src/routes/NavBar.svelte
+++ mentorme/src/routes/NavBar.svelte
@@ -14,15 +14,16 @@
   </script>
   
   <Navbar color="light" light>
-    <NavbarBrand href="/" class="me-auto">sveltestrap</NavbarBrand>
+    <NavbarBrand href="/" class="me-auto">MentorMe</NavbarBrand>
     <NavbarToggler on:click={toggle} class="me-2" />
     <Collapse {isOpen} navbar>
       <Nav navbar>
         <NavItem>
-          <NavLink href="#components/">Components</NavLink>
+          <!-- <NavLink href="#about/">about</NavLink> -->
+          <NavLink href="about/">About</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
+          <NavLink href="https://github.com/NeighbourhoodHacks3/FrontendSvelte">Frontend Code</NavLink>
         </NavItem>
       </Nav>
     </Collapse>
