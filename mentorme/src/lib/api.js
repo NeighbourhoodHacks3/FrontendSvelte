335f927ced31cbb6b89e661326481d13a7430fe7
Amelia <53657436+AmeliaTYR@users.noreply.github.com>
2023-05-21 16:55:33 +0800

    Update api.js

--- mentorme/src/lib/api.js
+++ mentorme/src/lib/api.js
@@ -87,7 +87,7 @@ export const createRequestPost = async (
 	/** @type {string} */ userID,
 	/** @type {string} */ title,
 	/** @type {string} */ description,
-	/** @type {string} */ tags,
+	/** @type {string[]} */ tags,
 	/** @type {string} */ location,
 	/** @type {string} */ payment
 ) => {
@@ -182,8 +182,196 @@ export const updateRequestPost = async (
 
 
 // API fucnction: create_request_comment
-// export const createRequestComment = async (
+export const createRequestComment = async (
+	/** @type {string} */ userID,
+	/** @type {string} */ requestPostID,
+	/** @type {string} */ comment
+) => {
+	try {
+		const bodyJson = {
+			userID: userID,
+			requestPostID: requestPostID,
+			comment: comment
+		};
 
+		const res = await axios.post('http://127.0.0.1:5000/requestComment/create', bodyJson, {
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*'
+			}
+		});
+
+		return res.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+// API fucnction: get_request_comments
+export const getRequestComments = async (
+	/** @type {string} */ requestPostID
+) => {
+	try {
+		const res = await axios.get('http://127.0.0.1:5000/requestComments', {
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*'
+			},
+			params: {
+				"requestPostID": requestPostID
+			}
+		});
+
+		return res.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+// API fucnction: create_offer_post
+export const createOfferPost = async (
+	/** @type {string} */ userID,
+	/** @type {string} */ title,
+	/** @type {string} */ description,
+	/** @type {string[]} */ tags,
+	/** @type {string} */ location,
+	/** @type {string} */ payment
+) => {
+	try {
+		const bodyJson = {
+			userID: userID,
+			title: title,
+			description: description,
+			tags: tags,
+			location: location,
+			payment: payment
+		};
+
+		const res = await axios.post('http://127.0.0.1:5000/offerPost/create', bodyJson, {
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*'
+			}
+		});
+
+		return res.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+// API fucnction: get_offer_post
+export const getOfferPost = async (
+	/** @type {string} */ offerPostID
+) => {
+	try {
+		const res = await axios.get('http://127.0.0.1:5000/offerPost', {
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*'
+			},
+			params: {
+				"offerPostID": offerPostID
+			}
+		});
+
+		return res.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+// API fucnction: get_offer_posts
+export const getOfferPosts = async (
+	/** @type {Number}} */ page
+) => {
+	try {
+		const res = await axios.get('http://127.0.0.1:5000/offerPosts', {
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*'
+			},
+			params: {
+				"page": page
+			}
+		});
+
+		return res.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+// API fucnction: update_offer_post
+export const updateOfferPost = async (
+	/** @type {string} */ offerPostID,
+	/** @type {Boolean} */ offerOpen,
+) => {
+	try {
+		const bodyJson = {
+			"offerPostID": offerPostID,
+			"offerOpen": offerOpen
+		};
+
+		const res = await axios.post('http://127.0.0.1:5000/offerPost/update', bodyJson, {
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*'
+			}
+		});
+
+		return res.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+// API fucnction: create_offer_comment
+export const createOfferComment = async (
+	/** @type {string} */ userID,
+	/** @type {string} */ offerPostID,
+	/** @type {string} */ comment
+) => {
+	try {
+		const bodyJson = {
+			userID: userID,
+			offerPostID: offerPostID,
+			comment: comment
+		};
+
+		const res = await axios.post('http://127.0.0.1:5000/offerComment/create', bodyJson, {
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*'
+			}
+		});
+
+		return res.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+// API fucnction: get_offer_comments
+export const getOfferComments = async (
+	/** @type {string} */ offerPostID
+) => {
+	try {
+		const res = await axios.get('http://127.0.0.1:5000/offerComments', {
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*'
+			},
+			params: {
+				"offerPostID": offerPostID
+			}
+		});
+
+		return res.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
 
 // API fucnction: login
 export const login = async (
