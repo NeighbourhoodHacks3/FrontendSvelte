68be537a644d57d0ddad34e46d19797305984fc9
e0425705 <puahxiuwen@gmail.com>
2023-05-21 18:38:27 +0800

    issue with staying on same page when error received

--- mentorme/src/routes/logout/+page.svelte
+++ mentorme/src/routes/logout/+page.svelte
@@ -1,16 +1,30 @@
 <script lang="ts">
 	import { login } from '$lib/api.js';
+	import logo from '$lib/images/logo.png';
+	import { goto } from '$app/navigation';
 
 	let email = document.getElementById('email');
 	let password = document.getElementById('password');
-  
+	let verified = false;
+
 	function enter() {
-	  if (email == null || password == null) {
-		alert('No such user details. Please re-enter details!')
-	  }
-	  else {
-		login(email.toString(), password.toString());
-	  }
+		if (email == null || password == null) {
+			alert('No such user details. Please re-enter details!');
+			//goto('/');
+			//window.location.href = "+page.svelte"
+			//verified = false;
+		} else {
+			login(email.toString(), password.toString());
+			//verified = true;
+		}
+
+		// if (verified == true) {
+		// 	login(email.toString(), password.toString());
+		// }
+
+		// if (email != null && password != null) {
+		// 	login(email.toString(), password.toString());
+		// }
 	}
 
 	function clearAll() {
@@ -18,41 +32,43 @@
 		password = null;
 	}
 </script>
-  
-  <svelte:head>
-	  <title>Login</title>
-	  <meta name="description" content="View Requests from Mentees" />
-  </svelte:head>
-  
-  <div style="text-align: center;" class="text-column;" on:load={clearAll}>
-	  <h1>LOGIN</h1>
 
-	  <div style="margin: 30px;"></div>
-  
-	  <!-- login stuffs -->
-	  <form action="/" method="post">
-		<div style="font-size: 25px;">
-		  <label for="email">Email</label>
-		  <input type="text" id="email" name="email" style="height: 35px; font-size: 22px;">
-		</div>
+<svelte:head>
+	<title>Login</title>
+	<meta name="description" content="View Requests from Mentees" />
+</svelte:head>
+
+<div style="text-align: center;" class="text-column;" on:load={clearAll}>
+	<img src={logo} alt="logo" />
+
+	<div style="margin: 30px;" />
 	
-		<div style="margin: 10px;"></div>
-	
+	<h1>LOGIN</h1>
+
+	<div style="margin: 30px;" />
+
+	<!-- login stuffs -->
+	<form action="/" method="post">
 		<div style="font-size: 25px;">
-		  <label for="password">Password</label>
-		  <input type="password" id="password" name="password" style="height: 35px; font-size: 22px;">
+			<label for="email">Email</label>
+			<input type="text" id="email" name="email" style="height: 35px; font-size: 22px;" />
 		</div>
 
-		<div style="margin: 20px;"></div>
-  
+		<div style="margin: 10px;" />
+
+		<div style="font-size: 25px;">
+			<label for="password">Password</label>
+			<input type="password" id="password" name="password" style="height: 35px; font-size: 22px;" />
+		</div>
+
+		<div style="margin: 20px;" />
+
 		<button class="btn btn-primary" on:click={enter}>Login</button>
-	  </form>
-  
-	  <div style="margin: 20px;"></div>
-  
-	  <nav>
-		<a style="font-size: 25px;" href="/signup">create new user</a><br><br>
-	  </nav>
-  
-  </div>
-  
+	</form>
+
+	<div style="margin: 20px;" />
+
+	<nav>
+		<a style="font-size: 25px;" href="/signup">create new user</a><br /><br />
+	</nav>
+</div>
