a56abca7cc8f86ac6ec6fb8213c7c2443d8bceb5
Amelia <53657436+AmeliaTYR@users.noreply.github.com>
2023-05-21 12:42:12 +0800

    Move the sverdle demo folder to archive

--- mentorme/src/lib/stores.js
+++ mentorme/src/lib/stores.js
@@ -1,16 +1,17 @@
 import { writable } from 'svelte/store';
 
-// var user = {
-//     name: "cyberyeet",
-//     email: "cyberyeet@gmail.com",
-//     userID: "6468ec339d23f8e8ae7ca59c",
-// };
+export const currentUser = writable({
+	name: 'cyberyeet',
+	email: 'cyberyeet@gmail.com',
+	userID: '6468ec339d23f8e8ae7ca59c',
+    password: 'superbadpassword1234'
+});
 
-export const currentUser = writable(
-    {
-        name: null,
-        email: null,
-        userID: null,
-        password: null
-    }
-);
+// export const currentUser = writable(
+//     {
+//         name: null,
+//         email: null,
+//         userID: null,
+//         password: null
+//     }
+// );
