bb099a7c58c061b6bcc66e894c0c1dcaba3e794b
Amelia <53657436+AmeliaTYR@users.noreply.github.com>
2023-05-21 20:42:59 +0800

    Clean up and landing page update

--- mentorme/src/routes/+page.svelte
+++ mentorme/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
-	import logo from '$lib/images/logo.png';
+	import logo from '$lib/images/mentormelogo.png';
 	import programmer from '$lib/images/programmer.png';
 	import teacher from '$lib/images/teacher.png';
 	import graphicdesigner from '$lib/images/graphicdesigner.png';
@@ -14,103 +14,84 @@
 </svelte:head>
 
 <body style="text-align: center;">
-	<img src={logo} alt="logo" />
+	<!-- Side by side div -->
+	<div class="headerDiv">
+		<img src={logo} alt="logo" style="height: 400px; " />
 
-	<div style="margin: 30px;" />
-
-	<!-- can use this to implement searchbar
-	https://framework7.io/svelte/searchbar -->
-
-	<!-- <input style="height: 50px; font-size: 35px;" type="text" placeholder="Seach..." />
-
-	<div style="margin: 30px;" /> -->
-
-	<div>
-		<img
-			style="padding: 10px; height: 300px; width: 300px; margin-right: 300px;"
-			src={programmer}
-			alt="programmer"
-		/>
-		<img style="padding: 10px; height: 300px; width: 300px;" src={teacher} alt="teacher" /><br />
-		<img
-			style="padding: 10px; height: 300px; width: 300px; margin-right: 300px;"
-			src={graphicdesigner}
-			alt="graphicdesigner"
-		/>
-		<img style="padding: 10px; height: 300px; width: 300px;" src={artist} alt="artist" />
+		<h2 class="bannerText">
+			Welcome to MentorMe, a platform for mentors and mentees to connect and learn from each other
+			on how to run a small business.
+		</h2>
 	</div>
 
-	<div style="margin: 30px;" />
+	<br />
+
+	<div class="info2">
+		<h1 class="">Find a mentor</h1>
+
+		<h2>Find a mentor who can help you with your small business</h2>
+
+		<div class="catPeopleDiv">
+			<img class="catPic" style="" src={programmer} alt="programmer" />
+			<img class="catPic" src={teacher} alt="teacher" /><br />
+			<img class="catPic" src={graphicdesigner} alt="graphicdesigner" />
+			<img class="catPic" src={artist} alt="artist" />
+		</div>
+	</div>
+
+	<br />
 
 	<nav>
-		<a style="font-size: 20px;" href="/about">need help?</a><br /><br />
+		<a style="font-size: 20px;" href="/about">Find out more!</a><br /><br />
 	</nav>
 </body>
 
-<!--
-<section>
-	<h1 style="font-style: italic; font-weight:bold">
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-	</h1>-->
-
-<!--<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>-->
-
-<!-- login stuffs -->
-<!--
-    <form action="/" method="post">
-      <div style="font-size: 25px;">
-        <label for="userId">User ID</label>
-        <input type="text" id="userId" name="userId" style="height: 25px; font-size: 22px;">
-      </div>
-  
-      <div style="margin: 10px;"></div>
-  
-      <div style="font-size: 25px;">
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" style="height: 25px; font-size: 22px;">
-      </div>
-    </form>
-
-	<br>
-
-	<nav>
-		<a style="font-size: 20px;" href="/profile">create new user</a><br><br>
-	</nav>
-</section>-->
-
 <style>
-	section {
+	.body {
+		height: 50%;
+		width: 100%;
+	}
+
+	.bannerText {
+		width: 50%;
+		margin: 10px;
+	}
+
+	.headerDiv {
+
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.info2 {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		flex: 0.6;
+		height: 350px;
+		background-color: rgb(199, 194, 248);
 	}
 
 	h1 {
-		width: 100%;
+		font-size: 2em;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	h2 {
+		font-size: 1.3em;
 	}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.catPic {
+		height: 150px;
+		width: 150px;
+		margin: 10px;
+	}
+
+	.catPeopleDiv {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
 	}
 </style>
